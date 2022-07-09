591fdaf92ad23da78a2c6dfd3210004c5a5f6d25
Jakob Goebel <jakob.goebel@avenga.com>
2022-07-09 13:48:21 +0200

    [server] Enable CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,14 @@
-import {NestFactory} from '@nestjs/core';
-import {env} from 'process';
-import {AppModule} from './app.module';
+import {NestFactory} from '@nestjs/core'
+import {env} from 'process'
+import {AppModule} from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(env.PORT ?? 8000);
+  const app = await NestFactory.create(AppModule)
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+  })
+
+  await app.listen(env.PORT ?? 8000)
 }
-bootstrap();
+bootstrap()
